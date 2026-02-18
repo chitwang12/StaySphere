cca52888b05b1728e346f3226620af5e6dfe084d
Chitwan Gandhi <chitwangandhi12@gmail.com>
2026-02-18 16:23:05 +0530

    Create and Get Hotel by Id Endpoint Created

---
+++ Hotel Service/src/routers/v1/hotel.router.ts
@@ -0,0 +1,12 @@
+import express from 'express';
+import { createHotelHandler, getHotelByIdHandler } from '../../controllers/hotel.controller';
+import { validateRequestBody } from '../../validators';
+import { hotelSchema } from '../../validators/hotel.validator'
+
+const hotelRouter = express.Router();
+
+hotelRouter.post('/', validateRequestBody(hotelSchema), createHotelHandler); // TODO: Resolve this TS compilation issue
+
+hotelRouter.get('/:id', getHotelByIdHandler);
+
+export default hotelRouter;
